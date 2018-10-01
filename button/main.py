cde04d3160f9e26ae637ac2a6b010fe214bf839b
mrks <yuke1222@gmail.com>
2018-10-02 00:51:06 +0900

    本日分

--- button/main.py
+++ button/main.py
@@ -4,48 +4,63 @@ import atexit
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
+import time
 
 MQTT_CLIENT = None
 
 CASH_PIN = 17
 SQUARE_PIN = 19
 PXVPAY_PIN = 21
-INPUT_PINS = [ CASH_PIN, SQUARE_PIN, PXVPAY_PIN ]
+BUTTON_PINS = [ CASH_PIN, SQUARE_PIN, PXVPAY_PIN ]
 
 LED1_PIN = 16
 LED2_PIN = 18
 LED3_PIN = 20
-OUTPUT_PINS = [ LED1_PIN, LED2_PIN, LED3_PIN ]
+LED_PINS = [ LED1_PIN, LED2_PIN, LED3_PIN ]
+LED_STATUS = [ False, False, False ]
+LED_UNHAPPY = False
+LED_UNHAPPY_TIMING = False
 
 
 def cleanup():
-	for pin in INPUT_PINS:
-		GPIO.remove_event_detect(pin)
+	for pin in BUTTON_PINS: GPIO.remove_event_detect(pin)
 	GPIO.cleanup()
 
 def pressHandler(pin):
-	if pin not in INPUT_PINS: return
+	if pin not in BUTTON_PINS: return
 	if MQTT_CLIENT is None: return
-	MQTT_CLIENT.publish('cart/checkout', INPUT_PINS.index(pin))
+	MQTT_CLIENT.publish('cart/checkout', BUTTON_PINS.index(pin))
 
+
+def ledctl():
+	if (not LED_UNHAPPY) or (LED_UNHAPPY and (not LED_UNHAPPY_TIMING)):
+		status = LED_STATUS
+	else:
+		if LED_STATUS.count(True) > 0:
+			status = [ False for i in range(len(LED_PINS)) ]
+		else:
+			status = [ len(LED_PINS)//2 == i for i in range(len(LED_PINS)) ]
+	for i in range(len(LED_PINS)): GPIO.output(LED_PINS[i], GPIO.HIGH if status[i] else GPIO.LOW)
+	LED_UNHAPPY_TIMING = not LED_UNHAPPY_TIMING
 
 def onConnect(client, userdata, flags, respons_code):
 	MQTT_CLIENT.subscribe('notice/cart')
 	MQTT_CLIENT.subscribe('notice/payment')
 
 def onMessage(client, userdata, msg):
-	if msg.topic === 'notice/cart':
+	if msg.topic == 'notice/cart':
 		count = 0
 		for item in json.loads(msg.payload)['items']: count += item['quantity']
-		for pin in OUTPUT_PINS[:count]: GPIO.output(channel, GPIO.HIGH)
-	elif msg.topic === 'notice/payment':
-		for pin in OUTPUT_PINS: GPIO.output(pin, GPIO.LOW)
-	elif msg.topic === 'notice/unlucky':
-		print('AAAAAAAAAA')
-		# メインでloop_foreverせずにずっとLEDの制御だけやってればいいのでは
+		LED_STATUS = [ i < count for i in range(len(LED_PINS)) ]
+	elif msg.topic == 'notice/payment':
+		LED_STATUS = [ False for i in range(len(LED_PINS)) ]
+	elif msg.topic == 'notice/unhappy':
+		LED_UNHAPPY = msg.payload
+		LED_UNHAPPY_TIMING = False
+	ledctl()
 
 
-if __name == '__main__':
+if __name__ == '__main__':
 	argv = sys.argv
 	argc = len(argv)
 	if argv != 1:
@@ -58,10 +73,10 @@ if __name == '__main__':
 
 
 	GPIO.setmode(GPIO.BCM)
-	for pin in INPUT_PINS:
+	for pin in BUTTON_PINS:
 		GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.add_event_detect(pin, GPIO.FALLING, callback=pressHandler, bouncetime=100)
-	for pin in OUTPUT_PINS:
+	for pin in LED_PINS:
 		GPIO.setup(pin, GPIO.OUT)
 		GPIO.output(pin, GPIO.LOW)
 	atexit.register(cleanup)
@@ -73,4 +88,7 @@ if __name == '__main__':
 		MQTT_CLIENT.connect(host)
 	else:
 		MQTT_CLIENT.connect(host, port=port)
-	MQTT_CLIENT.loop_forever()
+
+	while True:
+		ledctl()
+		time.sleep(0.5)
